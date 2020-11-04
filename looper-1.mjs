24e34e58bb110ddae05436e95d59e8836c247922
SoftWyer <github@softwyer.com>
2020-11-04 12:55:27 +0000

    Log test

---
+++ looper-1.mjs
@@ -0,0 +1,176 @@
+
+var lines = `
+Launching lib\main.dart on SM T510 in debug mode...
+ lib\main.dart
+√ Built build\app\outputs\flutter-apk\app-debug.apk.
+D/SurfaceView(30565): surfaceChanged (1200,1848) 1 #8 io.flutter.embedding.android.FlutterSurfaceView{f45eac7 V.E...... ......ID 0,0-1200,1848}
+D/OpenGLRenderer(30565): makeCurrent EglSurface : 0x0 -> 0xe151b3c0
+W/Gralloc3(30565): mapper 3.x is not supported
+I/gralloc (30565): Arm Module v1.0
+Connecting to VM Service at ws://127.0.0.1:55497/8AuixpvBmuE=/ws
+I/Choreographer(30565): Skipped 67 frames!  The application may be doing too much work on its main thread.
+D/ViewRootImpl@f6ee1[MainActivity](30565): Relayout returned: old=(0,0,1200,1920) new=(0,0,1200,1920) req=(1200,1920)0 dur=40 res=0x1 s={true 3152742400} ch=false
+D/ViewRootImpl@f6ee1[MainActivity](30565): MSG_WINDOW_FOCUS_CHANGED 1 1
+D/InputMethodManager(30565): prepareNavigationBarInfo() DecorView@1292165[MainActivity]
+D/InputMethodManager(30565): getNavigationBarColor() -855310
+D/InputMethodManager(30565): prepareNavigationBarInfo() DecorView@1292165[MainActivity]
+D/InputMethodManager(30565): getNavigationBarColor() -855310
+V/InputMethodManager(30565): Starting input: tba=com.XXX.XXX ic=null mNaviBarColor -855310 mIsGetNaviBarColorSuccess true , NavVisible : true , NavTrans : false
+D/InputMethodManager(30565): startInputInner - Id : 0
+I/InputMethodManager(30565): startInputInner - mService.startInputOrWindowGainedFocus
+D/ViewRootImpl@f6ee1[MainActivity](30565): MSG_RESIZED: frame=(0,0,1200,1920) ci=(0,36,0,72) vi=(0,36,0,72) or=1
+D/InputMethodManager(30565): prepareNavigationBarInfo() DecorView@1292165[MainActivity]
+D/InputMethodManager(30565): getNavigationBarColor() -855310
+V/InputMethodManager(30565): Starting input: tba=com.XXX.XXX ic=null mNaviBarColor -855310 mIsGetNaviBarColorSuccess true , NavVisible : true , NavTrans : false
+D/InputMethodManager(30565): startInputInner - Id : 0
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='0f96ae8a-1ab6-4968-91d3-79209bac0012', mState=SUCCEEDED, mOutputData=Data {}, mTags=[0dca6c56-cf76-4602-b0eb-4e0961cc7afe, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='118b37ff-8040-4659-8cf1-7670633a5869', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='14ff3f37-b9de-498e-83a5-be82622828ed', mState=SUCCEEDED, mOutputData=Data {}, mTags=[7d365335-fc13-40d9-baaa-a7da90f98ee5, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='180c1cb4-3e20-403d-98bc-e968d6c71cbb', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='1c9ad872-9d45-4e78-b4db-5370ec36bb94', mState=SUCCEEDED, mOutputData=Data {}, mTags=[b7520067-e369-4b3d-a86c-9cadf809c331, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='1cebd891-89cf-4169-83a8-6653a0d67391', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker, 12e8e0e0-91b1-44e9-ad97-e921f02a4fad], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='262db2ff-8cc5-4bbb-822d-308769281a78', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='2f9de106-e381-4bd1-b841-d8da7569e120', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='3110a122-3fc7-4901-9c29-b90dbf1f30b4', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='312c4a76-79a5-4cac-9fcf-e9df9e8c98de', mState=SUCCEEDED, mOutputData=Data {}, mTags=[2c74dca7-fef1-484d-9b4e-7c46eaf5f505, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='3af8d3c5-974b-4dad-8a89-3b2e07ea4f25', mState=SUCCEEDED, mOutputData=Data {}, mTags=[078fe194-2230-4532-afbe-591e02fdb320, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='3aff8307-4666-48d5-8da8-d01115fe0d08', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='3eaf7e31-fa5b-4ceb-ab92-a970ca6617e8', mState=SUCCEEDED, mOutputData=Data {}, mTags=[078fe194-2230-4532-afbe-591e02fdb320, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='4074d755-91f1-46cb-b96e-680b4ad3c0f2', mState=SUCCEEDED, mOutputData=Data {}, mTags=[078fe194-2230-4532-afbe-591e02fdb320, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='43843486-e340-4d5e-b0c8-12543befd358', mState=SUCCEEDED, mOutputData=Data {}, mTags=[7d365335-fc13-40d9-baaa-a7da90f98ee5, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='4a3bc950-2843-4d63-8873-7e6188920850', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='4dcd7708-4346-43a9-a18a-3505d524ff97', mState=SUCCEEDED, mOutputData=Data {}, mTags=[2c74dca7-fef1-484d-9b4e-7c46eaf5f505, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='4f590249-e062-4adf-b4cb-03d9c458e7e1', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='510392b5-eae5-4179-9aa3-b79720edd2bf', mState=SUCCEEDED, mOutputData=Data {}, mTags=[57550dba-ded8-402b-aa7d-6a5d331130f2, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='61db97ea-c7a5-458c-8942-258c4c828077', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='62117326-2306-4998-8628-3fb41c66f0ea', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='62c53410-1b40-449d-9cf7-3852bf7e7f4e', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='6bd9bbb4-a5c1-4399-a3fa-42b25138aded', mState=SUCCEEDED, mOutputData=Data {}, mTags=[2c74dca7-fef1-484d-9b4e-7c46eaf5f505, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='6f2aacf9-8e08-42a4-8655-b8e593aa2ae8', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='73b88d6f-2143-4487-bd59-f4384fd14c8e', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='745fd3f0-c3c2-409e-9cec-2669b2d39433', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='74bdc172-3ad0-4b46-9cb4-24547966cc02', mState=SUCCEEDED, mOutputData=Data {}, mTags=[7d365335-fc13-40d9-baaa-a7da90f98ee5, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='8c149690-9f71-4591-bdf0-3b3f0659a7c4', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='9a2af998-2746-4c92-9226-ef4054f661f9', mState=SUCCEEDED, mOutputData=Data {}, mTags=[c2368ebf-67c6-410d-b56a-d843f321c557, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='9fee7b5a-901a-4959-8003-255f8a9e7b15', mState=SUCCEEDED, mOutputData=Data {}, mTags=[0c96749d-2c9a-4709-b09d-11fda47708be, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='a446cd63-c8f5-4080-9b02-9792b1a4beed', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='a464db33-296a-454f-9dca-2b7b82ca52c1', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='ad1b9790-13d6-4165-be16-bbf678c86c1e', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='aecf3192-864d-470a-ba98-f12f76a31e30', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='aef621cf-fb18-493f-9fba-a0dad00ac42e', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='bffacc90-c3f4-49e4-8e51-2b718ebf5f90', mState=SUCCEEDED, mOutputData=Data {}, mTags=[e66f65b6-4f25-49a9-9146-f98d5be8bc91, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='c008e6b3-bf50-48c4-8141-5d1d4c4dd983', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='c128952a-b58d-4976-8056-5321ab1f38f0', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='c4ad2a18-11d1-499b-954b-6ea46d9feb9c', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='c81d1561-1800-461f-a36d-116977acdc49', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='d4797caa-d74d-4716-bc9a-b825c68e6016', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='d55bacb3-da96-4ca1-8d4f-f9698590b5b3', mState=SUCCEEDED, mOutputData=Data {}, mTags=[a82e1985-3666-4a14-aa3d-33b4d55fe88d, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='d99be536-3edf-480a-a271-09e302f7bd14', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='e9fa7aa6-4f10-48df-91c7-83355d56a45e', mState=SUCCEEDED, mOutputData=Data {}, mTags=[57550dba-ded8-402b-aa7d-6a5d331130f2, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='eb33bb96-634c-4a56-8d0e-2916aaa9e74a', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='ef8d810b-6640-4e4c-aaad-9d80eb3f0f55', mState=SUCCEEDED, mOutputData=Data {}, mTags=[a82e1985-3666-4a14-aa3d-33b4d55fe88d, be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='f40d9dfb-f70b-4180-a8d3-34797ddd2f3c', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='f9fc240e-762c-4243-ad84-d7b9f0f95174', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+I/System.out(30565): *** WorkInfo: WorkInfo{mId='ff19a8fa-addc-4ef4-9be3-9e9e8c5182f6', mState=SUCCEEDED, mOutputData=Data {}, mTags=[be.tramckrijte.workmanager.BackgroundWorker], mProgress=Data {}}
+D/Flutter Broadcast Receiver(30565): Action android.net.wifi.supplicant.STATE_CHANGE Intent is Intent { act=android.net.wifi.supplicant.STATE_CHANGE flg=0x24000010 (has extras) }
+D/Flutter Broadcast Receiver(30565): Supplicant state is COMPLETED
+D/Flutter Broadcast Receiver(30565): ssid            "Curly"
+D/Flutter Broadcast Receiver(30565): supplicantError null
+W/com.XXX.XXX(30565): Accessing hidden method Lsun/misc/Unsafe;->getUnsafe()Lsun/misc/Unsafe; (greylist,core-platform-api, linking, allowed)
+W/com.XXX.XXX(30565): Accessing hidden method Lsun/misc/Unsafe;->objectFieldOffset(Ljava/lang/reflect/Field;)J (greylist,core-platform-api, linking, allowed)
+W/com.XXX.XXX(30565): Accessing hidden method Lsun/misc/Unsafe;->compareAndSwapObject(Ljava/lang/Object;JLjava/lang/Object;Ljava/lang/Object;)Z (greylist, linking, allowed)
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+I/flutter (30565): ConfigServer: The stored config name is Dev Master
+D/FlutterSecureStoragePl(30565): Initializing StorageCipher
+D/FlutterSecureStoragePl(30565): StorageCipher initialization complete
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+I/chatty  (30565): uid=10433(com.XXX.XXX) identical 1 line
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+I/flutter (30565): Auth Service: Refreshing token entry
+I/flutter (30565): Auth Service: Calling token refresh with cached user 894197038
+I/System.out(30565): (HTTPLog)-Static: isSBSettingEnabled false
+I/System.out(30565): (HTTPLog)-Static: isSBSettingEnabled false
+I/System.out(30565): (HTTPLog)-Static: isSBSettingEnabled false
+I/System.out(30565): (HTTPLog)-Static: isSBSettingEnabled false
+I/flutter (30565): Auth Service: Calling token refresh result 833814596
+I/flutter (30565): [38;5;12m┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+[38;5;248mI/flutter (30565): [38;5;12m│ #0   AuthenticationService._refresh[39;49m
+ package:XXX/…/authentication/authentication_service.dart
+I/flutter (30565): [38;5;12m│ #1   <asynchronous suspension>
+I/flutter (30565): [38;5;12m├┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
+I/flutter (30565): [38;5;12m│ 💡 Token refresh successful Access token: eyJhbGciOiJS
+I/flutter (30565): [38;5;12m└───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+I/flutter (30565): [38;5;12m┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+[38;5;248mI/flutter (30565): [38;5;12m│ #0   AuthenticationService._refresh[39;49m
+ package:XXX/…/authentication/authentication_service.dart
+I/flutter (30565): [38;5;12m│ #1   <asynchronous suspension>
+I/flutter (30565): [38;5;12m├┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
+I/flutter (30565): [38;5;12m│ 💡 Valid until 2020-11-04 11:44:52.663
+I/flutter (30565): [38;5;12m└───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+I/flutter (30565): Auth Service: Token is not null
+I/flutter (30565): Auth Service: Token contains a valid user
+I/flutter (30565): Setting cached user 833814596 & timestamp 2020-11-04 11:39:52.772681
+I/flutter (30565): Auth Service: Refreshing token - complete, Instance of 'CurrentUser'
+I/flutter (30565): Auth Service: Current user token expires 5 minutes from now
+I/flutter (30565): Alarm Service: Going to refresh token 4 minutes from now
+I/FlutterBackgroundExecutor(30565): Starting AlarmService...
+I/AndroidAlarmManagerPlugin(30565): onAttachedToEngine
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+I/flutter (30565): Auth Service: Current user token expires 5 minutes from now
+I/flutter (30565): Alarm Service: Going to refresh token 4 minutes from now
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+W/com.XXX.XXX(30565): Accessing hidden method Lsun/misc/Unsafe;->compareAndSwapObject(Ljava/lang/Object;JLjava/lang/Object;Ljava/lang/Object;)Z (greylist, linking, allowed)
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+I/Choreographer(30565): Skipped 48 frames!  The application may be doing too much work on its main thread.
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+I/chatty  (30565): uid=10433(com.XXX.XXX) identical 2 lines
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+I/chatty  (30565): uid=10433(com.XXX.XXX) identical 1 line
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+V/Cursor  (30565): Filling cursor window with start position:0 required position:0
+I/flutter (30565): [38;5;12m┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+[38;5;248mI/flutter (30565): [38;5;12m│ #0   UploadRegistryService.resumeAllUploads[39;49m
+ package:XXX/…/upload/upload_registry_service.dart
+I/flutter (30565): [38;5;12m│ #1   <asynchronous suspension>
+I/flutter (30565): [38;5;12m├┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
+I/flutter (30565): [38;5;12m│ 💡 No uploads to resume
+I/flutter (30565): [38;5;12m└───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+D/ConnectivityManager(30565): requestNetwork; CallingUid : 10433, CallingPid : 30565
+I/AlarmService(30565): AlarmService started!
+I/flutter (30565): ServerCheck: Connected to Servers: 200
+I/flutter (30565): ServerCheck: Connected to Servers: 200
+  `;
+
+var linesArray = lines.split('\n');
+
+
+const logMessage = () => {
+  var count = Math.random() * 50;
+  var message = Date.now() + 'new log message: ';
+  for (var i = 0; i < count; i++) {
+    message += ' ' + Math.random();
+  }
+  console.log(message);
+};
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+
+
+export const looper = async () => {
+  var i = 0;
+  do {
+    // logMessage();
+    // console.log(linesArray[(Math.random() * linesArray.length) | 0]);
+    console.log(linesArray[i++ % linesArray.length]);
+    await sleep(Math.random(200) + 20 + (i % 20 == 0 ? 500 : 0));
+  } while (true);
+}
---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { looper as looper1 } from './looper-1.mjs';
+import { looper as looper2 } from './looper-with-a-long-file-name.mjs';
+
+looper1();
+looper2();
